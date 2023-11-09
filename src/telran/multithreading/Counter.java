be906e959da1440bf9bf9b56102c793b9f53a0dd
BorisKoba <browi@mail.ru>
2023-11-09 19:17:36 +0300

    CW #43 solution based on non-static mutexes

--- src/telran/multithreading/Counter.java
+++ src/telran/multithreading/Counter.java
@@ -1,21 +1,25 @@
 package telran.multithreading;
 
 public class Counter extends Thread {
- private static int counterResource1 = 0;
- private static int counterResource2 = 0;
- private final static Object mutex = new Object();
+ private CounterResource counterResource1;
+ private CounterResource counterResource2;
+
  private int nCounts;
  
- public Counter(int nCounts) {
+ 
+ 
+public Counter(CounterResource counterResource1, CounterResource counterResource2, int nCounts) {
+	this.counterResource1 = counterResource1;
+	this.counterResource2 = counterResource2;
 	this.nCounts = nCounts;
 }
- 
-public static int getCounterResource1() {
-	return counterResource1;
+
+public  int getCounterResource1() {
+	return counterResource1.getCounter();
 }
 
-public static int getCounterResource2() {
-	return counterResource2;
+public  int getCounterResource2() {
+	return counterResource2.getCounter();
 }
 
 @Override
@@ -25,13 +29,15 @@ public static int getCounterResource2() {
 		 count2();
 	 }
  }
-synchronized static private void count2() {
-	counterResource2++;
+private void count2() {
+	synchronized (counterResource2) {
+		counterResource2.increment();
+	}
 	
 }
-static private void count1() {
-	synchronized (mutex) {
-		counterResource1++;
+ private void count1() {
+	synchronized (counterResource1) {
+		counterResource1.increment();
 	}
 	
 }
