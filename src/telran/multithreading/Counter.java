5633601d51af1bf29149eee74eb337d22803d754
BorisKoba <browi@mail.ru>
2023-11-14 18:38:02 +0300

    Cw 44

--- src/telran/multithreading/Counter.java
+++ src/telran/multithreading/Counter.java
@@ -30,15 +30,15 @@ public  int getCounterResource2() {
 	 }
  }
 private void count2() {
-	synchronized (counterResource2) {
+	
 		counterResource2.increment();
-	}
+	
 	
 }
  private void count1() {
-	synchronized (counterResource1) {
+	 
 		counterResource1.increment();
-	}
+	
 	
 }
 }
